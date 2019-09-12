2a9e3bcb308492be9e6f081c6d4881aea4f6b28e
Michael Luo <shaochuanl@student.unimelb.edu.au>
2019-09-12 11:05:14 +1000

    1 Complete the jQuery part; 2 Add some comments.

--- resources/js/script.js
+++ resources/js/script.js
@@ -55,4 +55,48 @@ $(document).ready(function(){
           }
         });
     });
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        // 使用Animate.css网站上的各种动画效果只要
+        // 先加上animate修饰，再加上对应的动画修饰
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        // 整个页面的50%
+        offset: '50%'    
+    }) 
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    }) 
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    }) 
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    }) 
+
+    /* Mobile nav */
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        // 动画秒数为0.2秒, 神奇的函数，直接制定动画了
+        nav.slideToggle(200);
+
+        if(icon.hasClass('ion-navicon-round')){
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        }else{
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    })
 })
